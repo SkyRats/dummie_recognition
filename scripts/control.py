c35e52e19235165d0f76cefb4c2bdba717a59d69
GabiYuri <gabriela.ishikawa@gmail.com>
2020-11-24 20:18:23 -0300

    class DumDetect

--- scripts/control.py
+++ scripts/control.py
@@ -8,18 +8,23 @@ from cv_detection.msg import H_info
 
 height = 2
 
+def running_callback(state_data):
+    global running_state = state_data
+
 def run():
     
     rospy.init_node("head")
     mav = MAV("jorge")
     running_state = False
     running_state_pub = rospy.Publisher("/cv_detection/set_running_state", Bool, queue_size=10)
+    running_state_sub = rospy.Subscriber("/cv_detection/set_running_state", Bool, running_callback, queue_size=1)
     
     mav.takeoff(height)
     running_state = True
     running_state_pub.publish(running_state)
     mav.set_position(3, 3, height)
-
+    if running_state == True:
+        rospy.logwarn("ALL DUMMIES FOUND")
 
 if __name__ == "__main__":
     run()
--- src/thermal_detect.cpp
+++ src/thermal_detect.cpp
@@ -5,117 +5,53 @@
 #include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
-#include "thermal_detect.h"
+//#include "thermal_detect.h"
 #include "std_msgs/Bool.h"
 //#include "cv_detection/H_info.h"
+#define MAX_DUMMIES 1
 
 using namespace cv;
 
-//CLASS DEFINITIONS
-class Cam{
+//CLASS DEFINITION
+class DumDetect{
     private:
+        ros::NodeHandle n;
         ros::Subscriber cam_sub;
         void cam_callback(const sensor_msgs::ImageConstPtr& img);
-        
-    public:
-        Cam(ros::NodeHandle n);
-        ~Cam();
-        sensor_msgs::ImageConstPtr& cam_frame;
-};
-
-class Run{
-    private:
         ros::Subscriber run_sub;
         void running_callback(std_msgs::Bool data);
-        
+    
     public:
-        Run(ros::NodeHandle n);
-        ~Run();
+        DumDetect();
+        ~DumDetect();
+        sensor_msgs::ImageConstPtr& cam_frame;
         bool running_state; 
+        ros::Publisher run_pub;
+
+        Mat red(Mat frame);
+        Mat suficient_red(Mat frame);
+        bool allfound(Mat frame);
 };
 
-//CLASS IMPLEMENTATIONS
-Cam::Cam(ros::NodeHandle n)
+//CLASS IMPLEMENTATION
+DumDetect::DumDetect()
 {
-    this->cam_sub = n.subscribe("/iris_fpv_cam/usb_cam/image_raw", 5, &Cam::cam_callback, &cam_frame);
-}
-Cam::~Cam(){
-    std::cout << "Cam() destroyed" << std::endl;
+    this->cam_sub = this->n.subscribe("/iris_fpv_cam/usb_cam/image_raw", 5, &DumDetect::cam_callback, &cam_frame);
+    this->run_sub = this->n.subscribe("/cv_detection/set_running_state", 10, &DumDetect::running_callback, &running_state);
+    this->run_pub = this->n.advertise<std_msgs::Bool>("/cv_detection/set_running_state", 0);
 }
 
-Run::Run(ros::NodeHandle n)
+DumDetect::~DumDetect(){
+    std::cout << "DumDetect() destroyed" << std::endl;
+}
+
+//CALLBACKS 
+void DumDetect::running_callback(std_msgs::Bool data)
 {
-    this->run_sub = n.subscribe("/cv_detection/set_running_state", 10, &Run::running_callback, &running);
-    
-}
-Run::~Run(){
-    std::cout << "Run() destroyed" << std::endl;
+   this->running_state = data.data;
 }
 
-
-//MAIN 
-int main (int argc, char**argv){
-
-    int contador = 0;
-    Mat frame;
-    Mat imgred;
-    Mat imggray; 
-    
-
-    ros::init(argc, argv, "dummie_detection");
-
-    ros::NodeHandle n;
-    Run* run = new Run(n);
-    Cam* cam = new Cam(n);
-
-    ros::spin(); // trava o programa para rodar somente o callback
-    
-    bool running = run->running_state; 
-    
-    while (running == true) {
-        // Show each frame
-        frame = cam->cam_frame;
-        imshow("Camera", frame);
-
-        cvtColor(frame, imggray, COLOR_RGB2HSV);
-       
-        imgred = red(imggray);
-        
-        cvtColor(imgred, imggray, COLOR_RGB2GRAY);
-    
-        if (suficient_red(imggray)){
-            std::cout << "DUMMIE FOUND" << std::endl;
-            contador++;
-            //break;
-        }
-        if (contador == 1){
-            std::cout << "ALL DUMMIES HAVE BEEN FOUND" << std::endl;
-            break;
-        }
-        running = run->running_state; 
-    }
-    if (running == true){
-        // end of webcam
-        namedWindow("RED", CV_MINOR_VERSION);
-        namedWindow("GRAY", CV_MINOR_VERSION);
-        imshow("RED", imgred);
-        imshow("GRAY", imggray);
-        waitKey(0);
-
-    }
-    // end of red and gray image
-
-    return 0;
-};
-    
-//FUNCTIONS IMPLEMENTATIONS
-
-void Run::running_callback(std_msgs::Bool data)
-{
-   this->running_state = (data->data);
-}
-
-void Cam::cam_callback(const sensor_msgs::ImageConstPtr& img)
+void DumDetect::cam_callback(const sensor_msgs::ImageConstPtr& img)
 {
     try{
         this->cam_frame = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::RGB8);
@@ -126,6 +62,106 @@ void Cam::cam_callback(const sensor_msgs::ImageConstPtr& img)
         ROS_ERROR("cv_bridge exception: %s", e.what());
     }
 }
+    
+//FUNCTIONS IMPLEMENTATIONS
+Mat DumDetect::red(Mat frame){
+
+    Mat mask1, mask2, res1;
+    
+  // identify red images from frame.
+ //https://www.learnopencv.com/invisibility-cloak-using-color-detection-and-segmentation-with-opencv/
+
+    // creating mask
+    inRange(frame, Scalar(0, 120, 70), Scalar(10, 255, 255), mask1);
+    inRange(frame, Scalar(170, 120, 70), Scalar(180, 255, 255), mask2);
+
+    mask1 += mask2;
+
+    Mat kernel = Mat::ones(3,3, CV_32F);
+    morphologyEx(mask1,mask1,cv::MORPH_OPEN,kernel);
+    morphologyEx(mask1,mask1,cv::MORPH_DILATE,kernel);
+
+    // inverted mask
+    bitwise_not(mask1, mask2);
+
+    // red part
+    bitwise_and(frame, frame, res1, mask2);
+
+    return (res1);
+}
+
+bool DumDetect::suficient_red(Mat frame)
+{
+    int soma = 0; //number of red pixels
+    int MIN_RED = 10000;
+
+    for(int i = 1; i < frame.rows; i++){
+        for(int j = 1; j < frame.cols; j++){
+            if (frame.at<uchar>(i, j) >= 200){
+                soma++;
+            }
+        }
+    }
+    std::cout << soma << std::endl;    
+    if (soma > MIN_RED){
+        return true;
+    }
+    else
+        return false;
+
+}
+
+bool DumDetect::allfound(Mat frame){
+    int n = MAX_DUMMIES;
+    int contador = 0;
+    while (this->running_state == true) {
+        // Save each frame
+        imshow("Camera", frame);
+        Mat imggray;
+        Mat imgred;
+        
+        cvtColor(frame, frame, COLOR_RGB2HSV);
+       
+        imgred = this->red(frame);
+        
+        cvtColor(imgred, imggray, COLOR_RGB2GRAY);
+    
+        if (this->suficient_red(imggray)){
+            std::cout << "DUMMIE FOUND" << std::endl;
+            contador++;
+            //break;
+        }
+        if (contador == n){
+            std::cout << "ALL DUMMIES HAVE BEEN FOUND" << std::endl;
+            return true;
+        }
+        ros::spin();
+    }
+    return false;
+}
+
+//MAIN 
+int main (int argc, char**argv){
+
+    bool teste = false;
+    ros::init(argc, argv, "dummie_detection");
+    DumDetect* find = new DumDetect();
+    ros::spin(); // trava o programa para rodar somente o callback    
+    if(this->running_state)
+    {
+        teste = find->allfound(find->cam_frame);
+    }
+    if (teste)
+    {
+        find->run_pub.publish(teste);
+        std::cout << "MISSION COMPLETED" << std::endl;
+    }
+    else
+    {
+        std::cout<< "MISSION INCOMPLETE" << std::endl;
+    }
+    return 0;
+};
 
 
 // http://library.isr.ist.utl.pt/docs/roswiki/cv_bridge(2f)Tutorials(2f)ConvertingBetweenROSImagesAndOpenCVImagesPython.html
