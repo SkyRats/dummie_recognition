340048f0503979aca41b2793e1e693888944281a
GabiYuri <gabriela.ishikawa@gmail.com>
2021-01-19 20:10:11 -0300

    novo codigo

--- scripts/control.py
+++ scripts/control.py
@@ -4,58 +4,101 @@ import rospy
 from mavbase.MAV import MAV
 from std_msgs.msg import Bool
 from geometry_msgs.msg import TwistStamped, PoseStamped, Vector3Stamped
+import math
 
-from cv_detection.msg import H_info
 from dummie_recognition.msg import Running
 
 height = 2
 
-def running_callback(state_data):
-    global running_state
-    running_state = state_data.data
+def run_callback(state):
+    global run_state
+    run_state = state
 
-def teste_callback(state_data):
-    global teste_state
-    teste_state = state_data
+def pose_callback(pose):
+    global drone_pose
+    drone_pose = pose
+
+def dist(dummie, dummie_x, dummie_y, never_found):
+    if never_found:
+        rospy.loginfo("NERVER FOUND")
+        return 5
+
+    else:
+        dist_x = (dummie.pose_x - dummie_x)
+        dist_y = (dummie.pose_y - dummie_y)
+
+        dist_total = math.sqrt((dist_x**2 + dist_y**2))
+        rospy.loginfo("calculating dist")
+        return dist_total
 
 def run():
-    
+
     rospy.init_node("head")
     mav = MAV("jorge")
-    rate = rospy.Rate(60)
 
-    running_state_pub = rospy.Publisher("/cv_detection/set_running_state", Bool, queue_size=10)
-    running_state_sub = rospy.Subscriber("/cv_detection/set_running_state", Bool, running_callback, queue_size=1)
-
-    # NOVO TOPICO
-    teste_running_pub = rospy.Publisher("dummie_recognition/set_running_state", Running, queue_size=10)
-    teste_running_sub = rospy.Subscriber("dummie_recognition/set_running_state", Running, teste_callback, queue_size=1)
+    run_pub = rospy.Publisher("dummie_recognition/set_running_state", Running, queue_size = 10)
+    run_sub = rospy.Subscriber("dummie_recognition/set_running_state", Running, run_callback, queue_size = 1)
+    pose_sub = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, pose_callback)
 
     rospy.logwarn("CONTROL.PY RUNNING")
     mav.takeoff(height)
-    
-    teste = Running()
-    teste.search = True
-    teste.detected = True
-    teste.id = 76
-    teste.pose_x = 10
-    teste.pose_y = 80
+    mav.set_position(0, 0, height)
 
-    is_running = True
-    for i in range(10):
-        running_state_pub.publish(is_running)
-        teste_running_pub.publish(teste)
-        rate.sleep()
+    #NEW MSG
+    dummie = Running()
+    dummie.search = False
+    dummie.detected = False
+    dummie.id = 0
+    dummie.pose_x = 100
+    dummie.pose_y = 100
     
-    mav.set_position(-5, 5, height)
-    rospy.logwarn('NEW MSG = ' + str(teste_state))
-  
-#    while not rospy.is_shutdown():
-    if not running_state:
-        rospy.logwarn("NOT ALL DUMMIES WERE FOUND - control.py")
-    else:
-        rospy.logwarn("ALL DUMMIES WERE FOUND - control.py")
+    for i in range(10):
+        run_pub.publish(dummie)
+        mav.rate.sleep()
+    
+    # INICIALIZE DumDetect()
+    rospy.logwarn("INICIALIZING SEARCH")
+    for i in range(10):
+        dummie.search = True
+        run_pub.publish(dummie)
+        mav.rate.sleep()
+    
+    goal_pose_x = 3
+    goal_pose_y = 3
+    dummie_x = 0
+    dummie_y = 0
+
+    never_found = True
+    
+    while (abs(goal_pose_x - drone_pose.pose.position.x) > 0.01) and (abs(goal_pose_y - drone_pose.pose.position.y) > 0.01):
         
+        mav.set_position(goal_pose_x, goal_pose_y, height)
+        
+        if run_state.detected and dist(run_state, dummie_x, dummie_y, never_found) >= 1.5:
+            mav.hold(3)
+            rospy.logwarn('DUMMIE FOUND - ID = ' + str(run_state.id) + ' - py')
+            never_found = False
+            dummie_x = run_state.pose_x
+            dummie_y = run_state.pose_y
+
+        elif run_state.detected and dist(run_state, dummie_x, dummie_y, never_found) < 1.5:
+            rospy.loginfo("Detected, but too close")
+
+        else:
+            rospy.loginfo("Nothing detected")
+
+        dummie.search = True
+        run_pub.publish(dummie)
+        mav.rate.sleep()
+
+    # END DumDetect()
+    rospy.logwarn("ENDING SEARCH")
+    dummie.search = False
+    for i in range(10):
+        run_pub.publish(dummie)
+        mav.rate.sleep()
+
+    mav.land()
 
 if __name__ == "__main__":
     run()
--- src/dummie_detection.cpp
+++ src/dummie_detection.cpp
@@ -1,85 +1,102 @@
 #include <iostream>
-#include <ros/ros.h>
+using namespace std;
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
-#include <sensor_msgs/image_encodings.h>
+using namespace cv;
+
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h> 
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
 #include <geometry_msgs/PoseStamped.h>
-//#include "thermal_detect.h"
+
 #include "std_msgs/Bool.h"
 #include <cmath>
-//#include "cv_detection/H_info.h"
 #include "dummie_recognition/Running.h"
 
 #define MAX_DUMMIES 1
 #define MIN_DIST 1
 
-using namespace cv;
-
 //CLASS DEFINITION
-class DumDetect{
+class DumDetect
+{   
     private:
         ros::NodeHandle n;
+        
         ros::Subscriber cam_sub;
         void cam_callback(const sensor_msgs::ImageConstPtr& img);
-        ros::Subscriber run_sub;
-        void running_callback(std_msgs::Bool data);
+
         ros::Subscriber pose_sub;
         void pose_callback(geometry_msgs::PoseStamped pose);
-        //ros::Subscriber teste_sub;
-        //void teste_callback(dummie_recognition::Running info);
-    
+        geometry_msgs::PoseStamped actual_pose;
+
+        ros::Publisher run_pub;
+        ros::Subscriber run_sub;
+        void run_callback(dummie_recognition::Running state);
+        dummie_recognition::Running run_state;
+
     public:
         DumDetect();
         ~DumDetect();
-        Mat cam_frame;
-        bool running_state; 
-        bool change_position = true;
-        geometry_msgs::PoseStamped actual_pose;
-        dummie_recognition::Running teste_state;
-        ros::Publisher run_pub;
-        //ros::Publisher teste_pub;
 
         Mat red(Mat frame);
         bool suficient_red(Mat frame);
-        bool allfound(Mat frame);
-        //void teste_running(bool search, bool detected, int id);
+        bool found(Mat frame);
 };
 
 //CLASS IMPLEMENTATION
 DumDetect::DumDetect()
 {
+
     this->cam_sub = this->n.subscribe("/iris_fpv_cam/usb_cam/image_raw", 5, &DumDetect::cam_callback, this);
-    this->run_sub = this->n.subscribe("/cv_detection/set_running_state", 10, &DumDetect::running_callback, this);
-    this->run_pub = this->n.advertise<std_msgs::Bool>("/cv_detection/set_running_state", 1);
-    this->pose_sub = this->n.subscribe("/mavros/local_position/pose", 1, &DumDetect::pose_callback, this);
-    //this->teste_sub = this->n.subscribe("/dummie_recognition/set_running_state", 10, &DumDetect::teste_callback, this);
-    //this->teste_pub = this->n.advertise<dummie_recognition::Running>("/dummie_recognition/set_running_state", 1);
+    this->pose_sub = this->n.subscribe("mavros/local_position_pose", 10, &DumDetect::pose_callback, this);
+    this->run_sub = this->n.subscribe("/dummie_recognition/set_running_state", 10, &DumDetect::run_callback, this);
+    this->run_pub = this->n.advertise<dummie_recognition::Running>("/dummie_recognition/set_running_state", 10);
+
 }
 
-DumDetect::~DumDetect(){
+DumDetect::~DumDetect()
+{
     std::cout << "DumDetect() destroyed" << std::endl;
 }
 
-//CALLBACKS 
-void DumDetect::running_callback(std_msgs::Bool data)
-{
-   this->running_state = data.data;
-}
-
+//CALLBACKS
 void DumDetect::cam_callback(const sensor_msgs::ImageConstPtr& img)
 {
-    cv_bridge::CvImagePtr ros_img;
-    try{
-         
-        ros_img = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::RGB8);
+    if(this->run_state.search)
+    {
+       // ROS_WARN("IF CAM_CALLBACK - cpp");
+        cv_bridge::CvImagePtr ros_img;
+        try
+        {
+            ros_img = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::RGB8);
+            //this->cam_frame = ros_img->image;
+            //this->cam_frame = cv_bridge::toCvCopy(const sensor_msgs::Image& this->cam_frame);
+        }
         
-        this->cam_frame = ros_img->image;
-        //this->cam_frame = cv_bridge::toCvCopy(const sensor_msgs::Image& this->cam_frame);
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+        }
 
-    }catch (cv_bridge::Exception& e){
-        ROS_ERROR("cv_bridge exception: %s", e.what());
+        dummie_recognition::Running dummie;
+        if(this->found(ros_img->image) == true)
+        {
+            ROS_WARN("DUMMIE FOUND, CAM_CALLBACK - cpp");
+            dummie.pose_x = this->actual_pose.pose.position.x;
+            dummie.pose_y = this->actual_pose.pose.position.y;
+            dummie.detected = true;
+            dummie.id = dummie.id + 1;
+            for (int i = 0; i < 10; i++)
+            {
+                this->run_pub.publish(dummie);
+            }
+        }
+        else if (this->found(ros_img->image) == false)
+        {
+            ROS_WARN("DUMMIE NOT FOUND, CAM_CALLBACK - cpp");
+        } 
     }
 }
 
@@ -87,19 +104,16 @@ void DumDetect::pose_callback(geometry_msgs::PoseStamped pose)
 {
     this->actual_pose = pose;
 }
-/*
-void DumDetect::teste_callback(dummie_recognition::Running info)
-{
-    this->teste_state = info;
-}*/
-    
-//FUNCTIONS IMPLEMENTATIONS
-Mat DumDetect::red(Mat frame){
 
+void DumDetect::run_callback(dummie_recognition::Running state)
+{
+    this->run_state = state;
+}
+
+//FUNCTIONS IMPLEMENTATIONS
+Mat DumDetect::red(Mat frame)
+{
     Mat mask1, mask2, res1;
-    
-  // identify red images from frame.
- //https://www.learnopencv.com/invisibility-cloak-using-color-detection-and-segmentation-with-opencv/
 
     // creating mask
     inRange(frame, Scalar(0, 120, 70), Scalar(10, 255, 255), mask1);
@@ -117,13 +131,13 @@ Mat DumDetect::red(Mat frame){
     // red part
     bitwise_and(frame, frame, res1, mask2);
 
-    return (res1);
+    return res1;
 }
 
 bool DumDetect::suficient_red(Mat frame)
 {
     int soma = 0; //number of red pixels
-    int MIN_RED = 10000;
+    int MIN_RED = 4000;
 
     for(int i = 1; i < frame.rows; i++){
         for(int j = 1; j < frame.cols; j++){
@@ -132,130 +146,36 @@ bool DumDetect::suficient_red(Mat frame)
             }
         }
     }
-    //std::cout << soma << std::endl;    
+    //std::cout << soma << std::endl;
+    ROS_WARN("SOMA = %d", soma);
     if (soma > MIN_RED){
         return true;
     }
     else
         return false;
-
 }
 
- 
-bool DumDetect::allfound(Mat frame){
-    int n = MAX_DUMMIES, contador = 0;
-    geometry_msgs::PoseStamped drone_pose_initial;
-    geometry_msgs::PoseStamped drone_pose;
-    double dist_x, dist_y, dist, m = MIN_DIST;
-
-    while (this->running_state == true) {
-        // Save each frame
-        imshow("Camera", frame);
-        Mat imggray;
-        Mat imgred;
-                
-        cvtColor(frame, frame, COLOR_RGB2HSV);
-       
-        imgred = this->red(frame);
-        
-        cvtColor(imgred, imggray, COLOR_RGB2GRAY);
-        
+bool DumDetect::found(Mat frame){
     
-        if (change_position && this->suficient_red(imggray)){
+    cvtColor(frame, frame, COLOR_RGB2HSV);
+    frame = this->red(frame);
+    cvtColor(frame, frame, COLOR_RGB2GRAY);
 
-            //std::cout << "DUMMIE FOUND" << std::endl;
-            ROS_WARN( "DUMMIE FOUND ");
-            
-            drone_pose_initial = this->actual_pose;
-            contador++;
-            change_position = false;
-            //break;
-        }
-        if (contador == n){
-            //std::cout<<"ALL DUMMIES HAVE BEEN FOUND"<<std::endl;
-            ROS_INFO("ALL DUMMIES HAVE BEEN FOUND");
-            return true;
-        }
-        // MIN DISTANCE
-        drone_pose = this->actual_pose; 
-        dist_x = (drone_pose.pose.position.x - drone_pose_initial.pose.position.x);
-        dist_y = (drone_pose.pose.position.y - drone_pose_initial.pose.position.y);
-        
-        dist = dist_x * dist_x + dist_y * dist_y;
-        dist = sqrt(dist);
-        
-        if (dist > m) // definir a conditional
-        {
-            this->change_position = true;
-        }
-        ros::spin();
+    if(this->suficient_red(frame))
+    {
+        ROS_WARN("SUFICIENT RED TRUE - cpp");
+        return true;
     }
-    return false;
+    else
+    {
+        ROS_WARN("SUFICIENT RED FALSE - cpp");
+        return false;
+    }
 }
-/*
-void DumDetect::teste_running(bool search, bool detected, int id){
-    dummie_recognition::Running pub;
-    pub.search = search;
-    pub.detected = detected;
-    pub.id = id;
-    for (int i = 0; i < 10; i++){
-        this->teste_pub.publish(pub);
-    }
-    
-}*/
 
-//MAIN 
-int main (int argc, char**argv){
-
-    bool teste = true;        
-    ros::init(argc, argv, "dummie_detection");
-    DumDetect* find = new DumDetect();
-    /*
-    // NOVO TOPICO
-    bool search = true;
-    bool detected = false;
-    int id = 8;
-    find->teste_running(search, detected, id);
-*/
-    
-    while (!find->running_state){
-        ros::spin();
-    }
-    if(find->running_state)
-    {
-        ROS_WARN("IF DO RUNNING_STATE - cpp");
-        Mat img = find->cam_frame;
-        teste = find->allfound(img);
-    }
-    if (teste)
-    {
-        std_msgs::Bool teste_pub;
-        teste_pub.data = true;
-        find->run_pub.publish(teste_pub);
-        ROS_WARN("MISSION COMPLETED");
-    }
-    else if (!teste)
-    {
-        ROS_WARN("MISSION INCOMPLETE");
-    }
-    else 
-    {
-        ROS_WARN("????");
-    }
-
-    /*if(find->teste_state.search){
-        ROS_WARN("c++: search = true");
-    }
-
-    if(!find->teste_state.detected){
-        ROS_WARN("c++: detected = false");
-    }
-
-    ROS_WARN("c++: id = %d", find->teste_state.id);*/
-
-    ros::spin(); // trava o programa para rodar somente o callback    
-    delete find;
-    
-    return 0;
-
+int main(int argc, char** arvg){
+    ROS_WARN("RUNNING DUMMIE DETECTION");
+    ros::init(argc, arvg, "dummie_detection");
+    DumDetect* detect = new DumDetect();
+    ros::spin();
 };
